f84deaf257a37ace5d791be283b6c48f1222efae
Guillaume Lessard <dhtnstff@gmail.com>
2015-01-16 12:30:22 -0700

    Update to 'future<T>()'
    Now has approximately nothing to do with channels.

--- concurrency/future.swift
+++ concurrency/future.swift
@@ -7,20 +7,45 @@
 //
 
 import Darwin
+import Dispatch
 
-public func future<T>(task: () -> T) -> Receiver<T>
+/**
+  Execute a closure on a concurrent backround thread,
+  with the ability to retrieve its result at a later time.
+
+  The returned closure will allow retrieval of the original closure's result.
+  If the result is ready, the call will return immediately.
+  If the result is not ready yet, the call will block until the result can be returned.
+
+  The result can be used multiple times; the original closure is executed only once.
+
+  :param: task the closure to execute asynchronously.
+
+  :return: a new closure with the same return type, to retrieve the result of 'task'
+*/
+
+public func future<T>(task: () -> T) -> () -> T
 {
-  let (tx, rx) = Channel<T>.MakeSingleton()
+  let s = dispatch_semaphore_create(0)!
+  var r: T? = nil
 
   async {
-    tx <- task()
+    r = task()
+    dispatch_semaphore_signal(s)
   }
 
-  return rx
+  return {
+    dispatch_semaphore_wait(s, DISPATCH_TIME_FOREVER)
+    // Ensure the result is reusable by re-incrementing the semaphore
+    dispatch_semaphore_signal(s)
+    return r!
+  }
 }
 
 private func test()
 {
+  let deferredResult0: () -> Double = future { return 0.0 }
+
   let task = { () -> Int in
     sleep(1)
     return 10
@@ -32,7 +57,9 @@ private func test()
     return 20.0
   }
 
-  println("Result of work while waiting for deferred results")
-  println("Deferred result 1 is \(<-deferredResult1)")
-  println("Deferred result 2 is \(<-deferredResult2)")
+  println("Waiting for deferred results")
+  println("Deferred result 0 is \(deferredResult0())")
+  println("Deferred result 1 is \(deferredResult1())")
+  println("Deferred result 2 is \(deferredResult2())")
+  println("Deferred result 1 is \(deferredResult1())")
 }
