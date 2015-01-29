654587b089a3bb8929532d681d91b467d85c25b6
Guillaume Lessard <dhtnstff@gmail.com>
2015-01-29 00:05:25 -0700

    Improvement to merge-select.

--- concurrency/select/merge-select.swift
+++ concurrency/select/merge-select.swift
@@ -7,30 +7,31 @@
 //
 
 /**
-  Merge an array of channels into one Receiver
+  Merge an array of Receivers into one Receiver.
   Every item from the input channels will be able to be received via the returned channel.
-  This function uses a simple round-robin approach to merging channels; if any one
-  of the input channel blocks, the whole thing might block.
+
+  This function uses the select() function to merge channels; it will only block
+  if all channels block. The output channel will close when all inputs are closed.
 
   :param: channels an array of Receivers to merge.
 
   :return: a single Receiver provide access to get every message received by the input Receivers.
 */
 
-//public func merge<R: ReceiverType where R: Selectable>(channels: [R]) -> Receiver<R.ReceivedElement>
-public func merge<T>(channels: [Receiver<T>]) -> Receiver<T>
+public func merge<R: ReceiverType where R: Selectable>(channels: [R]) -> Receiver<R.ReceivedElement>
 {
   if channels.count == 0
   { // Not likely to happen, but return a closed channel.
-    return Receiver(Chan<T>())
+    return Receiver(Chan<R.ReceivedElement>())
   }
 
-  let (tx, rx) = Channel.Make(T.self, channels.count*2)
+  let (tx, rx) = Channel.Make(R.ReceivedElement.self, channels.count*2)
 
   async {
-    while let (s, selection) = select(channels)
+    let selectables = channels.map { $0 as Selectable }
+    while let (s, selection) = select(selectables)
     {
-      if let element: T = selection.getData()
+      if let element: R.ReceivedElement = selection.getData()
       {
         tx <- element
       }
