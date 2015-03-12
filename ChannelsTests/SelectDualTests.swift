838263e481e16a71c2bc1b9d95714cd4125be845
Guillaume Lessard <dhtnstff@gmail.com>
2015-03-11 18:04:53 -0600

    Test sets of channels sending and receiving via select()

---
+++ ChannelsTests/SelectDualTests.swift
@@ -0,0 +1,92 @@
+//
+//  SelectTests.swift
+//  Channels
+//
+//  Created by Guillaume Lessard on 2015-01-15.
+//  Copyright (c) 2015 Guillaume Lessard. All rights reserved.
+//
+
+import Darwin
+import Foundation
+import XCTest
+
+import Channels
+
+class SelectDualTests: XCTestCase
+{
+  private enum Sleeper { case Receiver; case Sender; case None }
+  
+  private func SelectTest(#buffered: Bool, sleeper: Sleeper)
+  {
+    let selectableCount = 10
+    let iterations = (sleeper == .None) ? 10_000 : 100
+    let sleepInterval = 0.01
+
+    let channels  = map(0..<selectableCount) { _ in Chan<Int>.Make(buffered ? 1 : 0) }
+    let senders   = channels.map { Sender($0) }
+    let receivers = channels.map { Receiver($0) }
+
+    async {
+      var i = 0
+      // Currently required to avoid a runtime crash:
+      let selectables = senders.map { $0 as Selectable }
+      while let selection = select(selectables)
+      {
+        if let sender = selection.id as? Sender<Int>
+        where sender.insert(selection, newElement: i)
+        {
+          i++
+          if sleeper == .Sender { NSThread.sleepForTimeInterval(sleepInterval) }
+        }
+        if i >= iterations { break }
+      }
+      for sender in senders { sender.close() }
+    }
+
+    var i = 0
+    // Currently required to avoid a runtime crash:
+    let selectables = receivers.map { $0 as Selectable }
+    while let selection = select(selectables)
+    {
+      if let message: Int = selection.getData()
+      {
+        i++
+        if sleeper == .Receiver { NSThread.sleepForTimeInterval(sleepInterval) }
+      }
+    }
+
+    syncprintwait()
+    
+    XCTAssert(i == iterations, "Received \(i) messages; expected \(iterations)")
+  }
+
+  func testUnbufferedDualSelect()
+  {
+    SelectTest(buffered: false, sleeper: .None)
+  }
+
+  func testBufferedDualSelect()
+  {
+    SelectTest(buffered: true, sleeper: .None)
+  }
+
+  func testUnbufferedSelectGet()
+  {
+    SelectTest(buffered: false, sleeper: .Sender)
+  }
+
+  func testBufferedSelectGet()
+  {
+    SelectTest(buffered: true, sleeper: .Sender)
+  }
+
+  func testUnbufferedSelectPut()
+  {
+    SelectTest(buffered: false, sleeper: .Receiver)
+  }
+
+  func testBufferedSelectPut()
+  {
+    SelectTest(buffered: true, sleeper: .Receiver)
+  }
+}
