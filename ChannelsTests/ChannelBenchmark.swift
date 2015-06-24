59bdc91818418f375ceeed52006b0d7fc593ed7a
Guillaume Lessard <dhtnstff@gmail.com>
2015-06-23 20:21:55 -0600

    Attempt to reproduce chan-benchmark as a test. Unfortunately slower than the command-line target.

---
+++ ChannelsTests/ChannelBenchmark.swift
@@ -0,0 +1,92 @@
+//
+//  ChannelBenchmark.swift
+//  Channels
+//
+//  Created by Guillaume Lessard on 2015-06-23.
+//  Copyright © 2015 Guillaume Lessard. All rights reserved.
+//
+
+import Darwin
+import XCTest
+
+@testable import Channels
+
+let iterations = 120_000
+
+class ChannelsBenchmark: XCTestCase
+{
+  func testBenchQBufferedChan()
+  {
+    let chan = Channel<Int>.Wrap(QBufferedChan(1))
+
+    let tic = Time()
+    for i in 0..<iterations
+    {
+      chan.tx <- i
+      if let _ = <-chan.rx {}
+    }
+    chan.tx.close()
+    let dt = tic.toc
+    print("\(dt)\t\t(\(dt/iterations) per message)")
+  }
+
+  func testBenchSBufferedChan()
+  {
+    let chan = Channel<Int>.Wrap(SBufferedChan(1))
+
+    let tic = Time()
+    for i in 0..<iterations
+    {
+      chan.tx <- i
+      if let _ = <-chan.rx {}
+    }
+    chan.tx.close()
+    let dt = tic.toc
+    print("\(dt)\t\t(\(dt/iterations) per message)")
+  }
+
+  func testBenchThreadedQBufferedChan()
+  {
+    let chan = Channel<Int>.Wrap(QBufferedChan(1))
+
+    let tic = Time()
+    async {
+      for i in 0..<iterations { chan.tx <- i }
+      chan.tx.close()
+    }
+
+    while let _ = <-chan.rx {}
+    let dt = tic.toc
+    print("\(dt)\t\t(\(dt/iterations) per message)")
+  }
+
+  func testBenchThreadedSBufferedChan()
+  {
+    let chan = Channel<Int>.Wrap(SBufferedChan(1))
+
+    let tic = Time()
+    async {
+      for i in 0..<iterations { chan.tx <- i }
+      chan.tx.close()
+    }
+
+    while let _ = <-chan.rx {}
+    let dt = tic.toc
+    print("\(dt)\t\t(\(dt/iterations) per message)")
+  }
+
+  func testBenchThreadedUnbufferedChan()
+  {
+    let chan = Channel<Int>.Make()
+
+    let tic = Time()
+    async {
+      for i in 0..<iterations { chan.tx <- i }
+      chan.tx.close()
+    }
+
+    while let _ = <-chan.rx {}
+    let dt = tic.toc
+    print("\(dt)\t\t(\(dt/iterations) per message)")
+  }
+}
