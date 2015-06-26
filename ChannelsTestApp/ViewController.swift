f7088c65b28e86987be5493852fab5f4e9f0f515
Guillaume Lessard <dhtnstff@gmail.com>
2015-06-26 13:22:12 -0600

    iOS app target so as to have tests run on a device.

---
+++ ChannelsTestApp/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  ChannelsTestApp
+//
+//  Created by Guillaume Lessard on 2015-06-26.
+//  Copyright © 2015 Guillaume Lessard. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view, typically from a nib.
+  }
+
+  override func didReceiveMemoryWarning() {
+    super.didReceiveMemoryWarning()
+    // Dispose of any resources that can be recreated.
+  }
+
+
+}
+
