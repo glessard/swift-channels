8bff0b86a4c4772a652d472255b0a1afe469358e
Guillaume Lessard <dhtnstff@gmail.com>
2015-06-26 03:58:39 -0600

    iOS framework target added to project.

---
+++ Channels_iOS/Channels_iOS.h
@@ -0,0 +1,19 @@
+//
+//  Channels_iOS.h
+//  Channels_iOS
+//
+//  Created by Guillaume Lessard on 2015-06-26.
+//  Copyright © 2015 Guillaume Lessard. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for Channels_iOS.
+FOUNDATION_EXPORT double Channels_iOSVersionNumber;
+
+//! Project version string for Channels_iOS.
+FOUNDATION_EXPORT const unsigned char Channels_iOSVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Channels_iOS/PublicHeader.h>
+
+
---
+++ Channels_iOSTests/Channels_iOSTests.swift
@@ -0,0 +1,35 @@
+//
+//  Channels_iOSTests.swift
+//  Channels_iOSTests
+//
+//  Created by Guillaume Lessard on 2015-06-26.
+//  Copyright © 2015 Guillaume Lessard. All rights reserved.
+//
+
+import XCTest
+
+class Channels_iOSTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock() {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
