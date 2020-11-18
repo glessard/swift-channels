45d06bee6fc053028955f0ce733df0912b5b74a0
Guillaume Lessard <dhtnstff@gmail.com>
2020-11-17 17:13:49 -0700

    handle future cases of `DispatchTimeInterval`

--- concurrency/dispatch-utilities.swift
+++ concurrency/dispatch-utilities.swift
@@ -15,7 +15,17 @@ extension DispatchTimeInterval
   var seconds: Double {
     @inline(__always)
     get {
-#if swift(>=3.2)
+#if swift(>=5.0)
+      switch self
+      {
+      case .nanoseconds(let ns):  return Double(ns)*1e-9
+      case .microseconds(let µs): return Double(µs)*1e-6
+      case .milliseconds(let ms): return Double(ms)*1e-3
+      case .seconds(let seconds): return Double(seconds)
+      case .never:                return Double.greatestFiniteMagnitude
+      @unknown default:           return Double.greatestFiniteMagnitude
+      }
+#elseif swift(>=3.2)
       switch self
       {
       case .nanoseconds(let ns):  return Double(ns)*1e-9
